5772361396f8f9492b15dd0454b33b92a37b9cb7
vzhyhunou <viktar_zhyhunou@epam.com>
2018-02-05 12:33:42 +0300

    app react component added

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>Hello</div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('react')
+);
