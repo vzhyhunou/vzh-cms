3d4ac47e55f8fb8fff7c9055c2ac3619707d97b2
vzhyhunou <viktar_zhyhunou@epam.com>
2018-02-06 19:02:24 +0300

    page controller test updated to use parameters

--- src/test/java/vzh/cms/controller/PageControllerTest.java
+++ src/test/java/vzh/cms/controller/PageControllerTest.java
@@ -1,11 +1,18 @@
 package vzh.cms.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.TestContextManager;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -16,20 +23,40 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * @author Viktar Zhyhunou
  */
-@RunWith(SpringRunner.class)
+@RunWith(Parameterized.class)
 @WebMvcTest(PageController.class)
 public class PageControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    public void index() throws Exception {
+    @Parameter
+    public String page;
 
-        this.mockMvc.perform(get("/"))
+    @Parameter(value = 1)
+    public String expected;
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        Collection<Object[]> params = new ArrayList<>();
+        params.add(new Object[]{"/", "built/index.js"});
+        params.add(new Object[]{"/pages", "built/pages.js"});
+        return params;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        TestContextManager testContextManager = new TestContextManager(getClass());
+        testContextManager.prepareTestInstance(this);
+    }
+
+    @Test
+    public void test() throws Exception {
+
+        this.mockMvc.perform(get(page))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(xpath("//body/script[1]/@src").string(equalTo("built/commons.js")))
-                .andExpect(xpath("//body/script[2]/@src").string(equalTo("built/index.js")));
+                .andExpect(xpath("//body/script[2]/@src").string(equalTo(expected)));
     }
 }
