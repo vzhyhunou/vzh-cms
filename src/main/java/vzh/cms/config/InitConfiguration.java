9d061086ce93acc1ba558f900c5c9e769893731f
vzhyhunou <viktar_zhyhunou@epam.com>
2018-04-27 01:11:23 +0300

    find repo by file name

--- src/main/java/vzh/cms/config/InitConfiguration.java
+++ src/main/java/vzh/cms/config/InitConfiguration.java
@@ -1,21 +1,24 @@
 package vzh.cms.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.support.Repositories;
 import vzh.cms.config.property.ApplicationProperties;
-import vzh.cms.model.Page;
-import vzh.cms.repository.PageRepository;
 
+import java.io.File;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Viktar Zhyhunou
@@ -27,16 +30,22 @@ public class InitConfiguration {
     private static final Logger LOG = LoggerFactory.getLogger(InitConfiguration.class);
 
     @Bean
-    public CommandLineRunner init(PageRepository pageRepository, ApplicationProperties properties) {
+    public CommandLineRunner init(ListableBeanFactory listableBeanFactory, ApplicationProperties properties) {
         return (args) -> {
 
             LOG.info("Initialization start");
 
+            Repositories repositories = new Repositories(listableBeanFactory);
             Path path = Paths.get(properties.getPage().getInit().getPath());
             ObjectMapper mapper = new ObjectMapper();
             try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
                 for (Path p : directoryStream) {
-                    pageRepository.save(Arrays.asList(mapper.readValue(p.toFile(), Page[].class)));
+                    File file = p.toFile();
+                    Class<?> c = Class.forName("vzh.cms.model." + file.getName().split("\\.")[0]);
+                    CollectionType type = mapper.getTypeFactory().constructCollectionType(List.class, c);
+                    Iterable values = mapper.readValue(file, type);
+                    CrudRepository repository = (CrudRepository) repositories.getRepositoryFor(c);
+                    repository.save(values);
                 }
             }
 
