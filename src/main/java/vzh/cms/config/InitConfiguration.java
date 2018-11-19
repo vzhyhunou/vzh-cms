79f4a5f40d78f42071c4fe1f2a8df5ddb44d1a1e
vzhyhunou <viktar_zhyhunou@epam.com>
2018-11-19 18:42:02 +0300

    update init config to use entity manager

--- src/main/java/vzh/cms/config/InitConfiguration.java
+++ src/main/java/vzh/cms/config/InitConfiguration.java
@@ -3,16 +3,15 @@ package vzh.cms.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.support.Repositories;
 import vzh.cms.config.property.CmsProperties;
 import vzh.cms.model.Content;
 import vzh.cms.repository.FileRepository;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -27,31 +26,35 @@ public class InitConfiguration {
     private static final Logger LOG = LoggerFactory.getLogger(InitConfiguration.class);
 
     @Bean
-    public CommandLineRunner init(ListableBeanFactory listableBeanFactory, CmsProperties properties, FileRepository fileRepository) {
-        return args -> {
+    public CommandLineRunner init(CmsProperties properties, FileRepository fileRepository, EntityManager manager) {
 
-            LOG.info("Initialization start");
+        return new CommandLineRunner() {
 
-            Repositories repositories = new Repositories(listableBeanFactory);
-            Path path = Paths.get(properties.getInit().getPath());
-            ObjectMapper mapper = new ObjectMapper();
-            try (DirectoryStream<Path> pathStream = Files.newDirectoryStream(path)) {
-                for (Path dir : pathStream) {
-                    Class<?> c = Class.forName(dir.toFile().getName());
-                    CrudRepository repository = (CrudRepository) repositories.getRepositoryFor(c);
-                    try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(dir)) {
-                        for (Path file : dirStream) {
-                            Object entity = mapper.readValue(file.toFile(), c);
-                            repository.save(entity);
-                            if (entity instanceof Content) {
-                                fileRepository.save(((Content) entity).getFiles());
+            @Override
+            @Transactional
+            public void run(String... args) throws Exception {
+
+                LOG.info("Initialization start");
+
+                Path path = Paths.get(properties.getInit().getPath());
+                ObjectMapper mapper = new ObjectMapper();
+                try (DirectoryStream<Path> pathStream = Files.newDirectoryStream(path)) {
+                    for (Path dir : pathStream) {
+                        Class<?> c = Class.forName(dir.toFile().getName());
+                        try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(dir)) {
+                            for (Path file : dirStream) {
+                                Object entity = mapper.readValue(file.toFile(), c);
+                                manager.persist(entity);
+                                if (entity instanceof Content) {
+                                    fileRepository.save(((Content) entity).getFiles());
+                                }
                             }
                         }
                     }
                 }
-            }
 
-            LOG.info("Initialization end");
+                LOG.info("Initialization end");
+            }
         };
     }
 }
