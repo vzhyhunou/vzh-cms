c7e7852fabf0881615e98fc9f32cf3603d7b3174
vzhyhunou <viktar_zhyhunou@epam.com>
2019-06-07 00:24:31 +0300

    interface updated

--- src/main/java/vzh/cms/repository/ContentRepository.java
+++ src/main/java/vzh/cms/repository/ContentRepository.java
@@ -3,13 +3,14 @@ package vzh.cms.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import vzh.cms.model.Content;
 
 import java.util.List;
 
 /**
  * @author Viktar Zhyhunou
  */
-public interface ContentRepository<T> extends Repository<T> {
+public interface ContentRepository<T extends Content> extends Repository<T> {
 
     <E> List<E> findAll(Specification<T> specification, Class<E> type, String locale);
 
