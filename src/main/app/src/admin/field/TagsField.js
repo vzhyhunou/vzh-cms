6fa0d93ba65617c6921ad3b4cd28926b6a71d042
vzhyhunou <viktar_zhyhunou@epam.com>
2019-06-18 12:09:39 +0300

    refactoring

--- src/main/app/src/admin/field/TagsField.js
+++ src/main/app/src/admin/field/TagsField.js
@@ -8,18 +8,17 @@ const styles = {
     chip: { margin: 4 },
 };
 
-const TagsField = ({ record, translate, resource }) => (
+const TagsField = ({record, translate, resource}) =>
     <span style={styles.main}>
-        {record.tags &&
-            record.tags.map(tag => (
-                <Chip
-                    key={tag}
-                    style={styles.chip}
-                    label={translate(`resources.${resource}.tags.${tag}`)}
-                />
-            ))}
+        {record.tags && record.tags.map(tag =>
+            <Chip
+                key={tag}
+                style={styles.chip}
+                label={translate(`resources.${resource}.tags.${tag}`)}
+            />
+        )}
     </span>
-);
+;
 
 const TranslatedTagsField = withTranslation(TagsField);
 
--- src/main/app/src/admin/input/TagsInput.js
+++ src/main/app/src/admin/input/TagsInput.js
@@ -3,16 +3,16 @@ import {SelectArrayInput} from 'react-admin';
 
 import {withSanitizedTranslation} from '../../commons/TranslationContext';
 
-const TagsInput = ({translate, addField, messages, resource, ...rest}) => (
+const TagsInput = ({addField, messages, resource, ...rest}) =>
     <SelectArrayInput
         {...rest}
         choices={Object.keys(messages.resources[resource].tags).map(tag => ({
             id: tag,
-            name: translate(`resources.${resource}.tags.${tag}`),
+            name: `resources.${resource}.tags.${tag}`
         }))}
         label={`resources.${resource}.fields.tags`}
     />
-);
+;
 
 const TranslatedTagsInput = withSanitizedTranslation(TagsInput);
 
