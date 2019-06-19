825640d6741060d7937876c49a1476bcdca6e656
vzhyhunou <viktar_zhyhunou@epam.com>
2019-06-19 19:11:02 +0300

    refactoring

--- src/main/java/vzh/cms/repository/ContentRepository.java
+++ src/main/java/vzh/cms/repository/ContentRepository.java
@@ -3,7 +3,6 @@ package vzh.cms.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import vzh.cms.model.Content;
 
 import java.util.List;
@@ -11,7 +10,7 @@ import java.util.List;
 /**
  * @author Viktar Zhyhunou
  */
-public interface ContentRepository<T extends Content> extends JpaSpecificationExecutor<T>, ItemRepository<T> {
+public interface ContentRepository<T extends Content> extends ItemRepository<T> {
 
     <E> List<E> findAll(Specification<T> specification, Class<E> type, String lang);
 
--- src/main/java/vzh/cms/repository/ItemRepository.java
+++ src/main/java/vzh/cms/repository/ItemRepository.java
@@ -1,10 +1,11 @@
 package vzh.cms.repository;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import vzh.cms.model.Item;
 
 /**
  * @author Viktar Zhyhunou
  */
-public interface ItemRepository<T extends Item> extends Repository<T> {
+public interface ItemRepository<T extends Item> extends JpaSpecificationExecutor<T>, Repository<T> {
 
 }
