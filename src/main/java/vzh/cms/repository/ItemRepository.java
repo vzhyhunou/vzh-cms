d6b0e54e28609b8fd96973fc87818b0ed1134c8c
vzhyhunou <viktar_zhyhunou@epam.com>
2019-06-19 21:10:12 +0300

    update repositories

--- src/main/java/vzh/cms/repository/ItemRepository.java
+++ src/main/java/vzh/cms/repository/ItemRepository.java
@@ -1,11 +1,10 @@
 package vzh.cms.repository;
 
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import vzh.cms.model.Item;
 
 /**
  * @author Viktar Zhyhunou
  */
-public interface ItemRepository<T extends Item> extends JpaSpecificationExecutor<T>, Repository<T> {
+public interface ItemRepository<T extends Item> extends Repository<T> {
 
 }
