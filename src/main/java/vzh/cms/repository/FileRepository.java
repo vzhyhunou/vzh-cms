6aebde9faf2cd80d9fa1e9983c39e47b19c81813
vzhyhunou <viktar_zhyhunou@epam.com>
2019-06-22 12:55:44 +0300

    refactoring

--- src/main/java/vzh/cms/repository/FileRepository.java
+++ src/main/java/vzh/cms/repository/FileRepository.java
@@ -1,7 +1,5 @@
 package vzh.cms.repository;
 
-import org.apache.commons.beanutils.BeanUtils;
-import org.apache.commons.io.FileUtils;
 import org.springframework.data.rest.core.mapping.ResourceMappings;
 import org.springframework.data.rest.core.mapping.ResourceMetadata;
 import org.springframework.stereotype.Repository;
@@ -11,16 +9,24 @@ import vzh.cms.model.Content;
 
 import java.io.File;
 import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Base64;
+
+import static java.nio.file.Files.exists;
+import static java.nio.file.Files.newDirectoryStream;
+import static org.apache.commons.beanutils.BeanUtils.getProperty;
+import static org.apache.commons.io.FileUtils.readFileToByteArray;
+import static org.apache.commons.io.FileUtils.writeByteArrayToFile;
 
 /**
  * @author Viktar Zhyhunou
  */
 @Repository
 public class FileRepository {
+
+    private static final Base64.Decoder DECODER = Base64.getDecoder();
+    private static final Base64.Encoder ENCODER = Base64.getEncoder();
 
     private String path;
 
@@ -35,20 +41,20 @@ public class FileRepository {
         File dir = location(content);
         for (Base64File file : content.getFiles()) {
             File out = new File(dir, file.getPath());
-            byte[] data = Base64.getDecoder().decode(file.getData());
-            FileUtils.writeByteArrayToFile(out, data);
+            byte[] data = DECODER.decode(file.getData());
+            writeByteArrayToFile(out, data);
         }
     }
 
     public void fill(Content content) throws Exception {
         Path dir = Paths.get(location(content).getPath());
-        if (Files.exists(dir)) {
-            try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(dir)) {
+        if (exists(dir)) {
+            try (DirectoryStream<Path> dirStream = newDirectoryStream(dir)) {
                 for (Path file : dirStream) {
                     Base64File f = new Base64File();
                     f.setPath(file.getFileName().toString());
-                    byte[] data = FileUtils.readFileToByteArray(file.toFile());
-                    f.setData(new String(Base64.getEncoder().encode(data)));
+                    byte[] data = readFileToByteArray(file.toFile());
+                    f.setData(new String(ENCODER.encode(data)));
                     content.getFiles().add(f);
                 }
             }
@@ -57,7 +63,7 @@ public class FileRepository {
 
     private File location(Object entity) throws Exception {
         ResourceMetadata meta = mappings.getMetadataFor(entity.getClass());
-        String id = BeanUtils.getProperty(entity, "id");
+        String id = getProperty(entity, "id");
         File dir = new File(meta.getRel(), id);
         return new File(path, dir.getPath());
     }
