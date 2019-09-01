aefce282ff2751b84504ecd5ca2a91ea65b285c3
vzhyhunou <viktar_zhyhunou@epam.com>
2019-09-01 22:30:51 +0300

    use meta model

--- src/main/java/vzh/cms/service/BaseService.java
+++ src/main/java/vzh/cms/service/BaseService.java
@@ -1,9 +1,12 @@
 package vzh.cms.service;
 
+import vzh.cms.model.Tag;
+import vzh.cms.model.Tag_;
+
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
-import java.sql.Timestamp;
 import java.util.Optional;
 
 /**
@@ -11,8 +14,10 @@ import java.util.Optional;
  */
 abstract public class BaseService {
 
-    protected static Predicate equal(CriteriaBuilder b, Expression<Number> expression, Number field) {
-        return field.intValue() > 0 ? b.equal(expression, field) : null;
+    protected static Predicate equal(CriteriaBuilder b, Expression<?> expression, Object field) {
+        return Optional.ofNullable(field)
+                .map(f -> b.equal(expression, f))
+                .orElse(null);
     }
 
     protected static Predicate like(CriteriaBuilder b, Expression<String> expression, String field) {
@@ -21,16 +26,16 @@ abstract public class BaseService {
                 .orElse(null);
     }
 
-    protected static Predicate in(Expression<Object[]> expression, Object[] fields) {
+    protected static Predicate in(Expression<?> expression, Object[] fields) {
         return Optional.ofNullable(fields)
                 .map(f -> expression.in(fields))
                 .orElse(null);
     }
 
-    protected static Predicate active(CriteriaBuilder b, Expression<Timestamp> start, Expression<Timestamp> end) {
+    protected static Predicate active(CriteriaBuilder b, Path<Tag> tag) {
         return b.and(
-                b.or(b.isNull(start), b.greaterThan(b.currentTimestamp(), start)),
-                b.or(b.isNull(end), b.lessThan(b.currentTimestamp(), end))
+                b.or(b.isNull(tag.get(Tag_.start)), b.greaterThan(b.currentTimestamp(), tag.get(Tag_.start))),
+                b.or(b.isNull(tag.get(Tag_.end)), b.lessThan(b.currentTimestamp(), tag.get(Tag_.end)))
         );
     }
 }
