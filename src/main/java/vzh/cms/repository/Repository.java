ac06ad44838bddc6bb6b6d08685cd18cf4f9898b
vzhyhunou <viktar_zhyhunou@epam.com>
2019-09-03 19:08:05 +0300

    update repositories

--- src/main/java/vzh/cms/repository/Repository.java
+++ src/main/java/vzh/cms/repository/Repository.java
@@ -1,8 +1,8 @@
 package vzh.cms.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.io.Serializable;
@@ -12,7 +12,7 @@ import java.util.List;
  * @author Viktar Zhyhunou
  */
 @NoRepositoryBean
-public interface Repository<T, ID extends Serializable> extends JpaRepository<T, ID>, JpaSpecificationExecutor<T>, CustomizedRepository<T> {
+public interface Repository<T, ID extends Serializable> extends PagingAndSortingRepository<T, ID>, JpaSpecificationExecutor<T>, CustomizedRepository<T> {
 
     List<T> findByIdIn(@Param("ids") ID[] ids);
 }
