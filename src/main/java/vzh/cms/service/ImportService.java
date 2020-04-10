b64035a6fa2b83d3544f8bb037a45998ab8d5d8b
vzhyhunou <viktar_zhyhunou@epam.com>
2020-04-10 12:42:42 +0300

    revert to repository usage

--- src/main/java/vzh/cms/service/ImportService.java
+++ src/main/java/vzh/cms/service/ImportService.java
@@ -1,14 +1,13 @@
 package vzh.cms.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 import vzh.cms.config.property.CmsImportProperties;
 import vzh.cms.config.property.CmsProperties;
 import vzh.cms.model.Content;
 
-import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -19,38 +18,31 @@ import java.nio.file.Paths;
  * @author Viktar Zhyhunou
  */
 @Service
-public class ImportService {
+public class ImportService extends MaintainService {
 
     private static final Logger LOG = LoggerFactory.getLogger(ImportService.class);
 
     private CmsImportProperties properties;
 
-    private FileService fileService;
-
-    private EntityManager manager;
-
-    private ObjectMapper mapper;
-
-    public ImportService(CmsProperties properties, FileService fileService, EntityManager manager, ObjectMapper mapper) {
+    public ImportService(CmsProperties properties) {
         this.properties = properties.getImp();
-        this.fileService = fileService;
-        this.manager = manager;
-        this.mapper = mapper;
     }
 
     @Transactional
+    @SuppressWarnings("unchecked")
     public void imp() throws Exception {
 
         Path path = Paths.get(properties.getPath());
         if (Files.exists(path)) {
             try (DirectoryStream<Path> pathStream = Files.newDirectoryStream(path)) {
                 for (Path dir : pathStream) {
-                    Class<?> type = Class.forName(dir.toFile().getName());
+                    Class<Object> type = (Class<Object>) Class.forName(dir.toFile().getName());
+                    CrudRepository<Object, ?> crudRepository = repository(type);
                     try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(dir)) {
                         for (Path file : dirStream) {
                             LOG.info("Import: {}", file);
                             Object entity = mapper.readValue(file.toFile(), type);
-                            manager.persist(entity);
+                            crudRepository.save(entity);
                             if (entity instanceof Content) {
                                 fileService.save((Content) entity);
                             }
