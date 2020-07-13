323ea06ee7a2220931ce94f41cb32dfbfb0e56c4
vzhyhunou <viktar_zhyhunou@epam.com>
2020-07-13 15:31:19 +0300

    two steps for inserting

--- src/main/java/vzh/cms/service/ImportService.java
+++ src/main/java/vzh/cms/service/ImportService.java
@@ -1,5 +1,6 @@
 package vzh.cms.service;
 
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.stereotype.Service;
 import vzh.cms.config.property.CmsImportProperties;
 import vzh.cms.config.property.CmsProperties;
@@ -16,6 +17,8 @@ import java.nio.file.Paths;
 @Service
 public class ImportService {
 
+    private static final String ID = "id";
+
     private CmsImportProperties properties;
 
     private MaintainService maintainService;
@@ -30,20 +33,27 @@ public class ImportService {
 
         Path path = Paths.get(properties.getPath());
         if (Files.exists(path)) {
-            imp(path);
+            imp(path, false);
+            imp(path, true);
         }
     }
 
-    private void imp(Path dir) throws Exception {
+    private void imp(Path dir, boolean full) throws Exception {
         try (DirectoryStream<Path> paths = Files.newDirectoryStream(dir)) {
             for (Path path : paths) {
                 if (path.toFile().isDirectory()) {
-                    imp(path);
+                    imp(path, full);
                 } else {
                     Object entity = maintainService.read(path.toFile());
-                    maintainService.getRepository(entity.getClass()).save(entity);
+                    maintainService.getRepository(entity.getClass()).save(full ? entity : getInstanceWithId(entity));
                 }
             }
         }
+    }
+
+    private Object getInstanceWithId(Object entity) throws Exception {
+        Object object = entity.getClass().newInstance();
+        new BeanWrapperImpl(object).setPropertyValue(ID, new BeanWrapperImpl(entity).getPropertyValue(ID));
+        return object;
     }
 }
