35fa332b14fe3366a48bb5b427c4c95994d23576
vzhyhunou <viktar_zhyhunou@epam.com>
2020-12-10 16:07:32 +0300

    improve search rest method

--- app/src/admin/rest.js
+++ app/src/admin/rest.js
@@ -114,6 +114,6 @@ export default (getLocale, getToken, apiUrl = '/api', httpClient = client(getLoc
 
     search: (resource, {path, query, options}) =>
         httpClient(`${apiUrl}/${resource}/search/${path}${query ? '?' + stringify(query) : ''}`, options).then(({json}) => ({
-            data: json
+            data: json && json._embedded ? json._embedded[resource] : json
         }))
 });
