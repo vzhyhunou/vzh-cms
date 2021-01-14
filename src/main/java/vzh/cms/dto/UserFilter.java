aee65c46da02cc3fae89bdd4570934bb18665584
vzhyhunou <viktar_zhyhunou@epam.com>
2021-01-14 15:04:32 +0300

    tagged filter parent

--- src/main/java/vzh/cms/dto/UserFilter.java
+++ src/main/java/vzh/cms/dto/UserFilter.java
@@ -1,14 +1,14 @@
 package vzh.cms.dto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author Viktar Zhyhunou
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class UserFilter {
+public class UserFilter extends TaggedFilter {
 
     private String id;
-
-    private String[] tags;
 }
