223f78641e26222d7f1b7463eba2dbb850598d0a
vzhyhunou <viktar_zhyhunou@epam.com>
2021-02-10 23:23:35 +0300

    update model compilation

--- src/main/java/vzh/cms/model/User.java
+++ src/main/java/vzh/cms/model/User.java
@@ -12,15 +12,17 @@ import java.io.Serializable;
 /**
  * @author Viktar Zhyhunou
  */
-@EqualsAndHashCode(callSuper = false)
+@EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
 @Data
-@ToString(exclude = "password")
+@ToString
 @Entity
 public class User extends Tagged<String> implements Serializable {
 
     @Id
+    @EqualsAndHashCode.Include
     private String id;
 
+    @ToString.Exclude
     @Size(min = 5)
     private String password;
 }
