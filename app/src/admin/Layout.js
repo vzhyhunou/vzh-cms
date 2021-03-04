df6169aaf8ef338533f6f85113905d2793c60006
vzhyhunou <viktar_zhyhunou@epam.com>
2021-03-04 22:51:22 +0300

    move configuration link to user menu

--- app/src/admin/Layout.js
+++ app/src/admin/Layout.js
@@ -1,6 +1,41 @@
-import React from 'react';
-import {Layout} from 'react-admin';
+import React, {useEffect, forwardRef} from 'react';
+import {
+    Layout,
+    AppBar,
+    UserMenu,
+    MenuItemLink,
+    useTranslate
+} from 'react-admin';
+import SettingsIcon from '@material-ui/icons/Settings';
 
-import Menu from './Menu';
+const ConfigurationMenu = forwardRef(({onClick}, ref) => {
 
-export default props => <Layout {...props} menu={Menu}/>;
+    const translate = useTranslate();
+
+    return <MenuItemLink
+        ref={ref}
+        to="/configuration"
+        primaryText={translate('pos.configuration')}
+        leftIcon={<SettingsIcon />}
+        onClick={onClick}
+    />;
+});
+
+const Menu = props =>
+    <UserMenu {...props}>
+        <ConfigurationMenu />
+    </UserMenu>
+;
+
+const Bar = props => <AppBar {...props} userMenu={<Menu />} />;
+
+export default props => {
+
+    const translate = useTranslate();
+
+    useEffect(() => {
+        document.title = translate('pos.title');
+    }, [translate]);
+
+    return <Layout {...props} appBar={Bar} />;
+};
