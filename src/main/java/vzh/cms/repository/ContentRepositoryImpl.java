69196823a65d7e9494b13b30530301bd12ccf3a5
vzhyhunou <viktar_zhyhunou@epam.com>
2021-03-28 21:26:08 +0300

    repositories refactoring

--- src/main/java/vzh/cms/repository/ContentRepositoryImpl.java
+++ src/main/java/vzh/cms/repository/ContentRepositoryImpl.java
@@ -2,15 +2,9 @@ package vzh.cms.repository;
 
 import org.springframework.context.i18n.LocaleContextHolder;
 import vzh.cms.model.Tagged;
-import vzh.cms.model.Tagged_;
 
 import javax.persistence.EntityManager;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.MapJoin;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import javax.persistence.criteria.Subquery;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
@@ -20,7 +14,6 @@ import java.util.Optional;
  */
 public abstract class ContentRepositoryImpl<T extends Tagged<ID>, ID extends Serializable> extends TaggedRepositoryImpl<T, ID> implements CustomizedContentRepository<T, ID> {
 
-    private static final String ID = "id";
     private static final String PROPERTIES = "properties";
 
     protected ContentRepositoryImpl(Class<T> domainClass, EntityManager manager) {
@@ -48,22 +41,5 @@ public abstract class ContentRepositoryImpl<T extends Tagged<ID>, ID extends Ser
                     b.equal(properties.key(), LocaleContextHolder.getLocale().getLanguage())
             );
         }, type);
-    }
-
-    protected Predicate filter(Root<T> root, CriteriaQuery<?> q, CriteriaBuilder b, Predicate p, Object... names) {
-        if (names.length == 0) {
-            return p;
-        }
-        Subquery<T> subquery = q.subquery(getDomainClass());
-        Root<T> r = subquery.from(getDomainClass());
-        return root.in(
-                subquery.select(r)
-                        .where(b.and(
-                                p,
-                                active(b, r.join(Tagged_.TAGS), names)
-                        ))
-                        .groupBy(r.get(ID))
-                        .having(b.equal(b.count(r.get(ID)), names.length))
-        );
     }
 }
