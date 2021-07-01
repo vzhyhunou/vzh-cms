cbe14220ae63b2b6b02d7e035c2296bc2948d54b
vzhyhunou <viktar_zhyhunou@epam.com>
2021-07-02 00:23:41 +0300

    update link field

--- app/src/admin/field/LinkField.js
+++ app/src/admin/field/LinkField.js
@@ -2,6 +2,6 @@ import React from 'react';
 import get from 'lodash/get';
 import {Link} from 'react-router-dom';
 
-export default ({source, name = source, record, resource}) =>
-    <Link to={`/cms/${resource}/${get(record, source)}`}>{get(record, name)}</Link>
+export default ({source = 'id', record, resource}) =>
+    <Link to={`/cms/${resource}/${get(record, 'id')}`}>{get(record, source)}</Link>
 ;
