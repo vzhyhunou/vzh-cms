2615bc9a232ae1be1d1056307b9bb1f7f1458521
vzhyhunou <viktar_zhyhunou@epam.com>
2021-07-05 00:07:34 +0300

    add parents for resource

--- app/src/admin/upload.test.js
+++ app/src/admin/upload.test.js
@@ -153,6 +153,71 @@ describe('upload', () => {
         })
     })
 
+    it('should modify request data with parents', () => {
+        expect.assertions(2)
+
+        window.FileReader = MockFileReader
+
+        const request = {
+            data: {
+                id: "sample.1",
+                parents: [2, "3"],
+                files: [
+                    {
+                        rawFile: new File([], "abc"),
+                        src: "blob:http://localhost:8090/cb822ba5-8864-4d03-97c2-a798cad9c7bc"
+                    },
+                    {
+                        rawFile: new File([], "abc"),
+                        src: "blob:http://localhost:8090/cb822ba5-8864-4d03-97c2-a798cad9c7bd"
+                    },
+                    {
+                        rawFile: new File([], "abe"),
+                        src: "blob:http://localhost:8090/cb822ba5-8864-4d03-97c2-a798cad9c7be"
+                    }
+                ],
+                file: {
+                    rawFile: new File([], "abf")
+                },
+                content: "<img src=\"blob:http://localhost:8090/cb822ba5-8864-4d03-97c2-a798cad9c7bc\"/>" +
+                "<img src=\"blob:http://localhost:8090/cb822ba5-8864-4d03-97c2-a798cad9c7bd\"/>" +
+                "<img src=\"blob:http://localhost:8090/cb822ba5-8864-4d03-97c2-a798cad9c7bd\"/>"
+            }
+        }
+        const expectedRequest = {
+            data: {
+                id: "sample.1",
+                parents: [2, "3"],
+                files: [
+                    {
+                        data: "abc",
+                        name: "900150983cd24fb0d6963f7d28e17f72.png"
+                    },
+                    {
+                        data: "abf",
+                        name: "ff905c528ce7ce9e64c0758b54855b50.png"
+                    }
+                ],
+                file: "ff905c528ce7ce9e64c0758b54855b50.png",
+                content: "<img src=\"/static/origin/items/2/3/sample/1/900150983cd24fb0d6963f7d28e17f72.png\"/>" +
+                "<img src=\"/static/origin/items/2/3/sample/1/900150983cd24fb0d6963f7d28e17f72.png\"/>" +
+                "<img src=\"/static/origin/items/2/3/sample/1/900150983cd24fb0d6963f7d28e17f72.png\"/>"
+            }
+        }
+
+        const response = {}
+
+        return addUploadFeature({
+            create: (resource, params) =>
+                new Promise(resolve => {
+                    expect(params).toEqual(expectedRequest)
+                    resolve(response)
+                })
+        }).create('items', request).then(r => {
+            expect(r).toEqual(response)
+        })
+    })
+
     it('no files in response', () => {
         expect.assertions(2)
 
@@ -219,6 +284,51 @@ describe('upload', () => {
         })
     })
 
+    it('should modify response item with parents', () => {
+        expect.assertions(2)
+
+        const request = {}
+
+        const response = {
+            data: {
+                id: "sample.1",
+                parents: [2, "3"],
+                files: [
+                    {
+                        name: "900150983cd24fb0d6963f7d28e17f72.png"
+                    }
+                ],
+                file: "900150983cd24fb0d6963f7d28e17f72.png"
+            }
+        }
+        const expectedResponse = {
+            data: {
+                id: "sample.1",
+                parents: [2, "3"],
+                files: [
+                    {
+                        src: "/static/origin/items/2/3/sample/1/900150983cd24fb0d6963f7d28e17f72.png",
+                        title: "900150983cd24fb0d6963f7d28e17f72.png"
+                    }
+                ],
+                file: {
+                    src: "/static/origin/items/2/3/sample/1/900150983cd24fb0d6963f7d28e17f72.png",
+                    title: "900150983cd24fb0d6963f7d28e17f72.png"
+                }
+            }
+        }
+
+        return addUploadFeature({
+            getOne: (resource, params) =>
+                new Promise(resolve => {
+                    expect(params).toEqual(request)
+                    resolve(response)
+                })
+        }).getOne('items', request).then(r => {
+            expect(r).toEqual(expectedResponse)
+        })
+    })
+
     it('should modify response items', () => {
         expect.assertions(2)
 
@@ -246,6 +356,51 @@ describe('upload', () => {
                 ],
                 file: {
                     src: "/static/origin/items/sample/1/900150983cd24fb0d6963f7d28e17f72.png",
+                    title: "900150983cd24fb0d6963f7d28e17f72.png"
+                }
+            }]
+        }
+
+        return addUploadFeature({
+            getList: (resource, params) =>
+                new Promise(resolve => {
+                    expect(params).toEqual(request)
+                    resolve(response)
+                })
+        }).getList('items', request).then(r => {
+            expect(r).toEqual(expectedResponse)
+        })
+    })
+
+    it('should modify response items with parents', () => {
+        expect.assertions(2)
+
+        const request = {}
+
+        const response = {
+            data: [{
+                id: "sample.1",
+                parents: [2, "3"],
+                files: [
+                    {
+                        name: "900150983cd24fb0d6963f7d28e17f72.png"
+                    }
+                ],
+                file: "900150983cd24fb0d6963f7d28e17f72.png"
+            }]
+        }
+        const expectedResponse = {
+            data: [{
+                id: "sample.1",
+                parents: [2, "3"],
+                files: [
+                    {
+                        src: "/static/origin/items/2/3/sample/1/900150983cd24fb0d6963f7d28e17f72.png",
+                        title: "900150983cd24fb0d6963f7d28e17f72.png"
+                    }
+                ],
+                file: {
+                    src: "/static/origin/items/2/3/sample/1/900150983cd24fb0d6963f7d28e17f72.png",
                     title: "900150983cd24fb0d6963f7d28e17f72.png"
                 }
             }]
