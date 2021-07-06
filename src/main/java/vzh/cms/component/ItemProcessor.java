75779a3ed71652e44cac23a7f354488a1c6495c2
vzhyhunou <viktar_zhyhunou@epam.com>
2021-07-07 01:09:22 +0300

    remove parents value when export

--- src/main/java/vzh/cms/component/ItemProcessor.java
+++ src/main/java/vzh/cms/component/ItemProcessor.java
@@ -8,6 +8,7 @@ import vzh.cms.model.Item;
 import vzh.cms.service.FileService;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * @author Viktar Zhyhunou
@@ -21,11 +22,12 @@ public class ItemProcessor implements RepresentationModelProcessor<EntityModel<I
     @Override
     public EntityModel<Item> process(EntityModel<Item> model) {
         Item item = model.getContent();
+        Objects.requireNonNull(item).setParents(item.parents());
         try {
             fileService.collect(item, false);
+            return model;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return model;
     }
 }
--- src/test/java/vzh/cms/service/LocationServiceTest.java
+++ src/test/java/vzh/cms/service/LocationServiceTest.java
@@ -49,7 +49,7 @@ public class LocationServiceTest {
 
         Item item = new Item() {
             @Override
-            public Object[] getParents() {
+            public Object[] parents() {
                 return new Object[]{1, "2.3"};
             }
         };
