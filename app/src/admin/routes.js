faadcfccff80eff62e4513ed180d97b7caae3f47
vzhyhunou <viktar_zhyhunou@epam.com>
2021-07-26 19:05:51 +0300

    pass parameters

--- app/src/admin/routes.js
+++ app/src/admin/routes.js
@@ -6,5 +6,5 @@ import Configuration from './Configuration';
 export default Layout => [
     <Route exact path="/" component={() => <Redirect to="cms/pages/home"/>}/>,
     <Route exact path="/configuration" component={Configuration}/>,
-    <Route path="/cms" render={() => <Layout/>} noLayout/>
+    <Route path="/cms/:resource/:id" component={({match}) => <Layout resource={match.params.resource} id={match.params.id}/>} noLayout/>
 ];
