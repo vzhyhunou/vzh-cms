0b265d7a76d3cbdd53bce1b2cd613a6fc3955edd
vzhyhunou <viktar_zhyhunou@epam.com>
2023-08-13 10:20:57 +0300

    refactoring

--- app/src/commons/locale.js
+++ app/src/commons/locale.js
@@ -1,17 +1,19 @@
 const LOCALE = 'locale';
 
+const setLocale = value => localStorage.setItem(LOCALE, value);
+const getLocale = () => localStorage.getItem(LOCALE) || 'en';
 const load = (i18n, locale) => i18n(locale).then(r => r.default);
 
 export default i18n => {
 
-    let locale = localStorage.getItem(LOCALE) || 'en', messages;
+    let locale = getLocale(), messages;
 
     return load(i18n, locale).then(m => {
         messages = m;
         return {
             getLocale: () => locale,
             getMessages: l => !l || l === locale ? messages : load(i18n, l).then(m => {
-                localStorage.setItem(LOCALE, l);
+                setLocale(l);
                 locale = l;
                 messages = m;
                 return m;
