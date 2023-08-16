acdc6f16588720bfcd3f356edec549da81d4dafc
vzhyhunou <viktar_zhyhunou@epam.com>
2023-08-16 18:39:18 +0300

    github pages

--- docs/static/js/613.d7c76d01.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_vzhyhunou_vzh_cms=self.webpackChunk_vzhyhunou_vzh_cms||[]).push([[613],{4613:function(n,t,a){a.r(t);var c=function(n){var t=n.parents,a=n.id;return((t?t.join("/")+"/":"")+a).replace(/\./g,"/")};t.default=function(){var n=arguments.length>0&&void 0!==arguments[0]?arguments[0]:"";return{originByData:function(t,a){return"".concat(n,"/static/origin/").concat(t,"/").concat(c(a))},pathByData:c}}}}]);
-//# sourceMappingURL=613.d7c76d01.chunk.js.map
