6f6ca6276b181adab8a02e7be9c30581dd05efce
vzhyhunou <viktar_zhyhunou@epam.com>
2023-08-25 21:44:38 +0300

    remove dependency to avoid deprecated arguments.callee usage

---
+++ app/src/commons/data/dumpKeys.js
@@ -0,0 +1,10 @@
+const dump = (obj, s = '') => Object.entries(obj)
+    .reduce((product, [k, v]) => {
+        const key =  s ? `${s}.${k}` : k;
+        if (Array.isArray(v)) {
+            return product.concat(dump({[key]: Object.fromEntries(v.map((v, i) => [i, v]))}));
+        }
+        return typeof v === 'object' ? product.concat(key).concat(dump(v, key)) : product.concat(key);
+    }, []);
+
+export default dump;
