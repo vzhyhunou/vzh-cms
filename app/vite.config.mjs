2360af37a2f675b7860addd4867d6e6d9eec8fc7
vzhyhunou <viktar_zhyhunou@epam.com>
2024-02-02 21:53:29 +0300

    fix vite config

--- app/vite.config.mjs
+++ app/vite.config.mjs
@@ -9,6 +9,9 @@ export default defineConfig({
         proxy: {
             '/api': {
                 target: 'http://localhost:8090'
+            },
+            '/login': {
+                target: 'http://localhost:8090'
             }
         }
     },
---
+++ docs/assets/index-007ac89a.js
@@ -0,0 +1,1 @@
+import{default as t}from"./en-628edcb8.js";import{default as f}from"./ru-8654b378.js";import"./index-5993d8ba.js";export{t as en,f as ru};
