10bc189a0ae998e606ab1336c58b8edf2fd5febe
vzhyhunou <viktar_zhyhunou@epam.com>
2024-02-21 21:48:51 +0300

    github pages

---
+++ docs/assets/index-89a7ed9d.js
@@ -0,0 +1,1 @@
+import{default as t}from"./back-43d4efbc.js";import{default as d}from"./fake-d62e1030.js";import"./index-e6fd6397.js";export{t as getDataBackProvider,d as getDataFakeProvider};
--- docs/assets/index-db7f0282.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"./back-50ff2d99.js";import{default as d}from"./fake-d9418911.js";import"./index-b8b2e837.js";export{t as getDataBackProvider,d as getDataFakeProvider};
