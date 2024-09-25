e0b10be668f5e9603b6603895d9888eaae45b7ae
vzhyhunou <viktar_zhyhunou@epam.com>
2024-09-25 11:48:29 +0300

    refactoring

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,22 @@
 import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
-import { CustomRoutes } from 'react-admin';
+import { Route } from 'react-router-dom';
 
-import {AdminLayout, Core, Page, PageComponent} from '.';
-import resources from './resources';
+import { Core, Page, PageComponent } from '.';
+import { pages, users } from '.';
 import Layout from './cms/Layout';
-
-const {pages, users} = resources;
 
 export default ({config}) => {
 
     const {resources: {users: {tags: {PAGES_EDITOR, MANAGER}}}} = config;
+    const resources = {
+        [PAGES_EDITOR]: [{pages}],
+        [MANAGER]: [{users}]
+    };
 
-    return <Core {...{config}} basename="/admin">
-        {permissions =>
-            <>
-                <CustomRoutes noLayout>
-                    <Route path="admin">
-                        {permissions ?
-                            <>
-                                <Route path="" element={<AdminLayout/>}/>
-                                {permissions.includes(PAGES_EDITOR) && <Route path="pages/*" element={<AdminLayout>{pages}</AdminLayout>}/>}
-                                {permissions.includes(MANAGER) && <Route path="users/*" element={<AdminLayout>{users}</AdminLayout>}/>}
-                            </>
-                            :
-                            <Route path="*?" element={<Navigate to="/login"/>}/>
-                        }
-                    </Route>
-                    <Route element={<Layout/>}>
-                        <Route path="" element={<PageComponent id="home" external/>}/>
-                        <Route path=":id" element={<Page/>}/>
-                    </Route>
-                </CustomRoutes>
-                {permissions &&
-                    <>
-                        {permissions.includes(PAGES_EDITOR) && pages}
-                        {permissions.includes(MANAGER) && users}
-                    </>
-                }
-            </>
-        }
+    return <Core {...{config, resources}}>
+        <Route element={<Layout/>}>
+            <Route path="" element={<PageComponent id="home" external/>}/>
+            <Route path=":id" element={<Page/>}/>
+        </Route>
     </Core>;
 };
